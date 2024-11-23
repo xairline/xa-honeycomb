7399d8a677366d8cb9c67e88fd046a804fdae6d9
Your Name <abc@myeamil.com>
2024-11-22 22:29:01 -0500

    init: clean up code, try a release run

--- pkg/xplane/xplane.go
+++ pkg/xplane/xplane.go
@@ -6,6 +6,8 @@ package xplane
 
 import (
 	"github.com/xairline/goplane/extra"
+	"github.com/xairline/goplane/extra/logging"
+	"github.com/xairline/goplane/xplm/menus"
 	"github.com/xairline/goplane/xplm/plugins"
 	"github.com/xairline/xa-honeycomb/pkg"
 	"github.com/xairline/xa-honeycomb/pkg/honeycomb"
@@ -25,9 +27,12 @@ type XplaneService interface {
 }
 
 type xplaneService struct {
-	Plugin       *extra.XPlanePlugin
-	BravoService honeycomb.BravoService
-	Logger       pkg.Logger
+	Plugin          *extra.XPlanePlugin
+	BravoService    honeycomb.BravoService
+	Logger          pkg.Logger
+	debug           bool
+	myMenuId        menus.MenuID
+	myMenuItemIndex int
 }
 
 var xplaneSvcLock = &sync.Mutex{}
@@ -44,8 +49,9 @@ func NewXplaneService(
 		xplaneSvcLock.Lock()
 		defer xplaneSvcLock.Unlock()
 		xplaneSvc := &xplaneService{
-			Plugin:       extra.NewPlugin("X Airline Snow - "+VERSION, "com.github.xairline.xa-snow", "show accumulated snow in X-Plane's world"),
+			Plugin:       extra.NewPlugin("xa honeycomb - "+VERSION, "com.github.xairline.xa-honeycomb", "honeycomb bridge"),
 			BravoService: honeycomb.NewBravoService(logger, "TODO"),
+			Logger:       logger,
 		}
 		xplaneSvc.Plugin.SetPluginStateCallback(xplaneSvc.onPluginStateChanged)
 		xplaneSvc.Plugin.SetMessageHandler(xplaneSvc.messageHandler)
@@ -75,8 +81,8 @@ func (s *xplaneService) onPluginStart() {
 	//if err != nil {
 	//	s.Logger.Errorf("Some error occured. Err: %s", err)
 	//}
-	//if os.Getenv("OVERRIDE") == "true" {
-	//	s.override = true
+	//if os.Getenv("DEBUG") == "true" {
+	//	s.debug = true
 	//}
 	//
 	//// API drefs are available at plugin start
@@ -91,16 +97,16 @@ func (s *xplaneService) onPluginStart() {
 	//// start with delay to let the dust settle
 	//processing.RegisterFlightLoopCallback(s.flightLoop, 5.0, nil)
 	//
-	//// setup menu
-	//menuId := menus.FindPluginsMenu()
-	//menuContainerId := menus.AppendMenuItem(menuId, "X Airline Snow", 0, false)
-	//s.myMenuId = menus.CreateMenu("X Airline Snow", menuId, menuContainerId, s.menuHandler, nil)
-	//s.myMenuItemIndex = menus.AppendMenuItem(s.myMenuId, "Toggle Override", 0, true)
-	//if s.override {
-	//	menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Checked)
-	//} else {
-	//	menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Unchecked)
-	//}
+	// setup menu
+	menuId := menus.FindPluginsMenu()
+	menuContainerId := menus.AppendMenuItem(menuId, "XA Honeycomb", 0, false)
+	s.myMenuId = menus.CreateMenu("XA Honeycomb", menuId, menuContainerId, s.menuHandler, nil)
+	s.myMenuItemIndex = menus.AppendMenuItem(s.myMenuId, "Enable Debug", 0, true)
+	if s.debug {
+		menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Checked)
+	} else {
+		menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Unchecked)
+	}
 	//
 	//// set internal vars to known "no snow" state
 	//s.snowNow, s.rwySnowCover, s.iceNow = s.p2x.SnowDepthToXplaneSnowNow(0)
@@ -118,77 +124,6 @@ func (s *xplaneService) flightLoop(
 	ref interface{},
 ) float32 {
 
-	// flightloop start is the first point in time where the time datarefs are set correctly
-	//if s.loopCnt == 0 {
-	//	s.loopCnt++
-	//	s.Logger.Info("Flightloop starting, kicking off")
-	//
-	//	// delayed init
-	//	if !initDrefs(s) {
-	//		return 0 // Bye, if we don't have them by now we will never get them
-	//	}
-	//
-	//	sys_time := dataAccess.GetIntData(s.sysTime_dr) == 1
-	//	day := dataAccess.GetIntData(s.simCurrentDay_dr)
-	//	month := dataAccess.GetIntData(s.simCurrentMonth_dr)
-	//	hour := dataAccess.GetIntData(s.simLocalHours_dr)
-	//
-	//	go func() {
-	//		for {
-	//			err := honeycomb.bravoSvc.DownloadAndProcessBravoFile(sys_time, month, day, hour)
-	//			if err != nil {
-	//				s.Logger.Errorf("Download bravo file failed: %v", err)
-	//			} else {
-	//				// TODO: was this looping forever?
-	//				break
-	//			}
-	//			// TODO: disabled - auto NOAA update
-	//		}
-	//	}()
-	//
-	//	return 10.0
-	//}
-	//
-	//if !s.override {
-	//	weatherMode := dataAccess.GetIntData(s.weatherMode_dr)
-	//	if weatherMode != 1 {
-	//		// weather mode is not RW, we don't do anything to avoid snow on people's summer view
-	//		return 5.0
-	//	}
-	//}
-	//
-	//if !s.BravoService.IsReady() {
-	//	return 2.0
-	//}
-	//
-	//// throttle update computations
-	//s.loopCnt++
-	//if s.loopCnt%8 == 0 {
-	//	lat := dataAccess.GetFloatData(s.lat_dr)
-	//	lon := dataAccess.GetFloatData(s.lon_dr)
-	//	snowDepth_n := s.BravoService.GetSnowDepth(lat, lon)
-	//
-	//	// some exponential smoothing
-	//	const alpha = float32(0.7)
-	//	s.snowDepth = alpha*snowDepth_n + (1-alpha)*s.snowDepth
-	//
-	//	// If we have no accumulated snow leave the datarefs alone and let X-Plane do its weather effects
-	//	if s.snowDepth < 0.001 && !s.override {
-	//		return -1
-	//	}
-	//
-	//	s.snowNow, s.rwySnowCover, s.iceNow = s.p2x.SnowDepthToXplaneSnowNow(s.snowDepth)
-	//}
-	//
-	//// If we have no accumulated snow leave the datarefs alone and let X-Plane do its weather effects
-	//if s.snowDepth < 0.001 && !s.override {
-	//	return -1
-	//}
-	//
-	//dataAccess.SetFloatData(s.snow_dr, s.snowNow)
-	//dataAccess.SetFloatData(s.rwySnowCover_dr, s.rwySnowCover)
-	//dataAccess.SetFloatData(s.ice_dr, s.iceNow)
-
 	return -1
 }
 
@@ -199,21 +134,15 @@ func (s *xplaneService) messageHandler(message plugins.Message) {
 }
 
 func (s *xplaneService) menuHandler(menuRef interface{}, itemRef interface{}) {
-	//s.override = !s.override
-	//
-	//if s.override {
-	//	menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Checked)
-	//} else {
-	//	menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Unchecked)
-	//}
-	//
-	//// write to config
-	//err := godotenv.Write(map[string]string{
-	//	"OVERRIDE": strconv.FormatBool(s.override),
-	//}, s.configFilePath)
-	//if err != nil {
-	//	s.Logger.Errorf("Error writing to config: %v", err)
-	//}
+	s.debug = !s.debug
+
+	if s.debug {
+		logging.MinLevel = logging.Debug_Level
+		menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Checked)
+	} else {
+		logging.MinLevel = logging.Info_Level
+		menus.CheckMenuItem(s.myMenuId, s.myMenuItemIndex, menus.Menu_Unchecked)
+	}
 
 	s.Logger.Infof("menu clicked: %v", itemRef)
 }
