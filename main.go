c86423f79acbfbcc0252e657f55c0d724a1b5d3c
Your Name <abc@myeamil.com>
2024-11-29 18:44:15 -0500

    hard code the start window size for UI

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "bravo",
-		Width:  1024,
-		Height: 768,
+		Width:  1200,
+		Height: 1200,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
